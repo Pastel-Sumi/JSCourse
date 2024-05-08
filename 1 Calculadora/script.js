63f13f1411f66a2e4dc55324e53e310c526de95a
Pastel-Sumi <ral.prismriver@outlook.com>
2024-05-08 19:27:47 -0400

    Inicio proyecto 1 js

---
+++ 1 Calculadora/script.js
@@ -0,0 +1,13 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item)=>{
+    item.onclick=()=>{
+        if(item.id == "clear"){
+            display.innerText = "";
+        }
+        else if ( item.id == "backspace"){
+            
+        }
+    }
+})
