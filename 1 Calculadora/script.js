24a576a4fc1694839551ad57d5b55fd4fe78ebeb
Pastel-Sumi <ral.prismriver@outlook.com>
2024-05-08 19:47:07 -0400

    setTimeout

--- 1 Calculadora/script.js
+++ 1 Calculadora/script.js
@@ -10,5 +10,15 @@ buttons.forEach((item)=>{
             let string = display.innerText.toString();
             display.innerText = string.substr(0,string.length-1);
         }
-    }
-})
+        else if (display.innerText != "" && item.id == "equal"){
+            display.innerText = eval(display.innerText);
+        }
+        else if (display.innerText =="" && item.id =="equal"){
+            display.innerText = "Null";
+            setTimeout(()=>(display.innerText=""), 2000);
+        }
+        else{
+            display.innerText += item.id;
+        }
+    };
+});
