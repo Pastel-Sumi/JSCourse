9f61b70dd3d6f28b0a9385b981ed7ab60d2ca24a
Pastel-Sumi <ral.prismriver@outlook.com>
2025-12-17 16:02:45 -0300

    Proyecto 21 Pinocho parallax

---
+++ 21 Pinocho Parallax/script.js
@@ -0,0 +1,27 @@
+const cards = document.querySelector(".cards");
+const images = document.querySelectorAll(".card__img");
+const backgrounds = document.querySelectorAll(".card__bg");
+const range = 40;
+
+//funcion pora calcular el valor de la posición de nuestro mouse
+const calcValue = (a, b) =>( (a/b)*range - (range/2)).toFixed(1);
+    
+
+
+let timeout;
+document.addEventListener("mousemove",({x,y}) => {
+    if(timeout){
+        window.cancelAnimationFrame(timeout);
+    }
+    timeout = window.requestAnimationFrame(() =>{
+        const yValue = calcValue(y,window.innerHeight);
+        const xValue = calcValue(x, window.innerWidth);
+        cards.style.transform = `rotateX(${yValue}deg)rotateY(${xValue}deg)`;
+        [].forEach.call(images,(image) =>{
+            image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
+        });
+        [].forEach.call(backgrounds, (item) =>{
+            item.style.backgroundPosition = `${xValue* 0.45}px ${-yValue * 0.45}px`;
+        })
+    });
+}, false);
